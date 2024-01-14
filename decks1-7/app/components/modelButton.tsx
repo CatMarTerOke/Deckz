8fd3287bea954adf6dd75636b1b97aaa20eb509e
CatMarTerOke <98166823+CatMarTerOke@users.noreply.github.com>
2024-01-14 13:31:03 -0500

    trying to make components, but can't find console logs

---
+++ decks1-7/app/components/modelButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react'
+
+
+const ModelButton = () => {
+    return(
+        <div>
+            <button onClick = {() => console.log('click')}>
+                modelButtonClick
+            </button>
+        </div>
+    )
+}
+
+export default ModelButton
--- decks1-7/app/page.tsx
+++ decks1-7/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ModelButton from './components/modelButton'
 
 
-export default function Page() {
+export default function Home() {
   return (
     <>
     <main> <h1> this is the main page</h1></main>
