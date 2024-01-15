ea1e568d70d42eac0a89d6b6f9386e26aefd6ce6
CatMarTerOke <98166823+CatMarTerOke@users.noreply.github.com>
2024-01-14 21:27:58 -0500

    images, flexbox with center alognment, images as links

--- decks1-7/app/page.tsx
+++ decks1-7/app/page.tsx
@@ -1,13 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import ModelButton from './components/modelButton'
+import './globals.css'; 
+
 
 
 export default function Home() {
+  
   return (
     <>
-    <main> <h1> this is the main page</h1></main>
-    <Link href="newUser"> new User </Link>
+      <Image src = '/images/DecksTopBlock.png' width= {1500} height={500} alt="topBlock"/>
+      
+      <section>
+        <Link href="newUser">
+          <Image src='/images/newUserButton.png' width={400} height={200} alt="newUserButton"/>
+        </Link>
+        <Image src='/images/oldUserButton.png' width={400} height={200} alt="oldUserButton"/>
+      </section>
+    
     
     </>
     
